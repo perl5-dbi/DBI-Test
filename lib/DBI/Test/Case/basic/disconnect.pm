ce3913f7d269596d660a3f89acdac2781c27dc17
Jens Rehsack <sno@netbsd.org>
2013-07-10 09:45:33 +0200

    perltidy

--- lib/DBI/Test/Case/basic/disconnect.pm
+++ lib/DBI/Test/Case/basic/disconnect.pm
@@ -2,103 +2,106 @@ use strict;
 use warnings;
 use Test::More;
 
-our @DB_CREDS = ('dbi:SQLite::memory:', undef, undef, {});
+our @DB_CREDS = ( 'dbi:SQLite::memory:', undef, undef, {} );
 my %SQLS = (
-  'SELECT' => 'SELECT 1+1',
-  'INSERT' => undef
-);
+             'SELECT' => 'SELECT 1+1',
+             'INSERT' => undef
+           );
 
-{ #Basic test
-  my $dbh = DBI->connect( @DB_CREDS );
-  isa_ok($dbh, 'DBI::db');
-  
-  ok($dbh->disconnect(), "Disconnect");
-  
-  #Disconnect should clear the active flag of a database handle
-  ok(!$dbh->{Active}, 'dbh is inactive');
+{    #Basic test
+    my $dbh = DBI->connect(@DB_CREDS);
+    isa_ok( $dbh, 'DBI::db' );
+
+    ok( $dbh->disconnect(), "Disconnect" );
+
+    #Disconnect should clear the active flag of a database handle
+    ok( !$dbh->{Active}, 'dbh is inactive' );
 }
 
-{ #Test that disconnect prints a warning if it disconncets on an active statementhandler
-  #Q: Does it print an warning even though PrintWarn is false?
-  my $dbh = DBI->connect( @DB_CREDS );
-  isa_ok($dbh, 'DBI::db');
-  
-  #Create  statementhandler
-  my $sth = $dbh->prepare($SQLS{SELECT}); #TODO : some SELECT should go inside here, or?
-  isa_ok($sth, 'DBI::st');
-  ok($sth->execute(), 'execute');
-  
-  my $warnings = 0;
-  
-  #Make sure we fetch the local
-  local $SIG{__WARN__} = sub {
-    $warnings++ if( shift() =~ m/^DBI::db/ );
-  };  
-  
-  #The statementhandler should have more rows to fetch
-  ok($dbh->disconnect(), "Disconnect");
-  cmp_ok($warnings, '>', 0, "Catched a warning");
-}
+{    #Test that disconnect prints a warning if it disconncets on an active statementhandler
+        #Q: Does it print an warning even though PrintWarn is false?
+    my $dbh = DBI->connect(@DB_CREDS);
+    isa_ok( $dbh, 'DBI::db' );
 
-{ #Negative test
-  #TODO how should we force it to fail. Mock DBD\DBI?
-  
-  TODO : {
-    local $TODO = "Must make an API to make the disconnecct fail";
-    my $dbh = DBI->connect( @DB_CREDS );
-    isa_ok($dbh, 'DBI::db');
-  
-    #Put code to make disconnect fail in here
-    ok(!$dbh->disconnect(), "Disconnect failure");
-    #Check that $DBI::err && $DBI::errstr is set
-    #It should be set after a failed call
-    ok($DBI::err, '$DBI::err is set');
-    ok($DBI::errstr, '$DBI::errstr is set');
-    
-    #Disconnect failed. The Active flag should still be true
-    ok($dbh->{Active}, 'dbh is still active');
-  }
-  
-}
+    #Create  statementhandler
+    my $sth = $dbh->prepare( $SQLS{SELECT} );    #TODO : some SELECT should go inside here, or?
+    isa_ok( $sth, 'DBI::st' );
+    ok( $sth->execute(), 'execute' );
 
-{ #Check that disconnect does print an error when PrintError is true
-  #TODO how should we force it to fail. Mock DBD\DBI?
-  
-  TODO : {
-    local $TODO = "Must make an API to make the disconnecct fail";
-    my $dbh = DBI->connect( @DB_CREDS[0..2], { PrintError => 1 });
-    isa_ok($dbh, 'DBI::db');
-  
-    my @warnings = ();
+    my $warnings = 0;
+
     #Make sure we fetch the local
     local $SIG{__WARN__} = sub {
-        my ($called_from, $warning) = @_;  # to find out Carping methods
-        my $warn_kind = $called_from eq 'Carp' ? 'carped' : 'warn';
-        my @warning_stack = split /\n/, $warning;     # some stuff of uplevel is included
-        push(@warnings, $warning_stack[0]);
-    };   
-  
-    #TODO : force disconncet to fail
-  
-    #Put code to make disconnect fail in here
-    ok(!$dbh->disconnect(), "Disconnect failure");
-    cmp_ok(scalar(@warnings), '>', 1, "Warning recorded");
-  }
+        $warnings++ if ( shift() =~ m/^DBI::db/ );
+    };
+
+    #The statementhandler should have more rows to fetch
+    ok( $dbh->disconnect(), "Disconnect" );
+    cmp_ok( $warnings, '>', 0, "Catched a warning" );
 }
 
-{ #Check that disconnect does dies on fail when RaiseError is set
-  #TODO how should we force it to fail. Mock DBD\DBI?
-  
-  TODO : {
-    local $TODO = "Must make an API to make the disconnecct fail";
-    my $dbh = DBI->connect( @DB_CREDS[0..2], { RaiseError => 1 });
-    isa_ok($dbh, 'DBI::db');
-     
-    #TODO : force disconncet to fail
-  
-    #Put code to make disconnect fail in here
-    eval { $dbh->disconnect(); };
-    ok($@, "Disconnect raised error"); 
-  }
+{    #Negative test
+        #TODO how should we force it to fail. Mock DBD\DBI?
+
+  TODO:
+    {
+        local $TODO = "Must make an API to make the disconnecct fail";
+        my $dbh = DBI->connect(@DB_CREDS);
+        isa_ok( $dbh, 'DBI::db' );
+
+        #Put code to make disconnect fail in here
+        ok( !$dbh->disconnect(), "Disconnect failure" );
+        #Check that $DBI::err && $DBI::errstr is set
+        #It should be set after a failed call
+        ok( $DBI::err,    '$DBI::err is set' );
+        ok( $DBI::errstr, '$DBI::errstr is set' );
+
+        #Disconnect failed. The Active flag should still be true
+        ok( $dbh->{Active}, 'dbh is still active' );
+    }
+
+}
+
+{    #Check that disconnect does print an error when PrintError is true
+        #TODO how should we force it to fail. Mock DBD\DBI?
+
+  TODO:
+    {
+        local $TODO = "Must make an API to make the disconnecct fail";
+        my $dbh = DBI->connect( @DB_CREDS[ 0 .. 2 ], { PrintError => 1 } );
+        isa_ok( $dbh, 'DBI::db' );
+
+        my @warnings = ();
+        #Make sure we fetch the local
+        local $SIG{__WARN__} = sub {
+            my ( $called_from, $warning ) = @_;    # to find out Carping methods
+            my $warn_kind = $called_from eq 'Carp' ? 'carped' : 'warn';
+            my @warning_stack = split /\n/, $warning;    # some stuff of uplevel is included
+            push( @warnings, $warning_stack[0] );
+        };
+
+        #TODO : force disconncet to fail
+
+        #Put code to make disconnect fail in here
+        ok( !$dbh->disconnect(), "Disconnect failure" );
+        cmp_ok( scalar(@warnings), '>', 1, "Warning recorded" );
+    }
+}
+
+{    #Check that disconnect does dies on fail when RaiseError is set
+        #TODO how should we force it to fail. Mock DBD\DBI?
+
+  TODO:
+    {
+        local $TODO = "Must make an API to make the disconnecct fail";
+        my $dbh = DBI->connect( @DB_CREDS[ 0 .. 2 ], { RaiseError => 1 } );
+        isa_ok( $dbh, 'DBI::db' );
+
+        #TODO : force disconncet to fail
+
+        #Put code to make disconnect fail in here
+        eval { $dbh->disconnect(); };
+        ok( $@, "Disconnect raised error" );
+    }
 }
 done_testing();
