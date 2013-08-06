6e1317251e05342cc2872d732d756bb892bef122
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2013-08-06 13:05:37 +0200

    Don't force PURE_PERL DBI

--- lib/DBI/Mock.pm
+++ lib/DBI/Mock.pm
@@ -483,7 +483,7 @@ sub _miss_dbi
     defined $_have_dbi and return !$_have_dbi;
     $_have_dbi = 0;
     eval qq{
-	\$ENV{DBI_PUREPERL} = 2; # we only want to know if it's there ...
+	#\$ENV{DBI_PUREPERL} = 2; # we only want to know if it's there ...
 	require DBI;
 	\$_have_dbi = 1;
     };
