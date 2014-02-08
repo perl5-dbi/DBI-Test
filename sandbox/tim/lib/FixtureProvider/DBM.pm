bbffa698aacc682e136575c502c9971bf9c525e6
Tim Bunce <Tim.Bunce@pobox.com>
2014-02-08 21:59:48 +0000

    Added temp table name generation. This works now: perl tumbler.pl && prove -j4 -sr out

--- sandbox/tim/lib/FixtureProvider/DBM.pm
+++ sandbox/tim/lib/FixtureProvider/DBM.pm
@@ -11,18 +11,20 @@ sub get_ro_stmt_select_1r2c_si {
 }
 
 sub get_ro_stmt_select_1r2c_si_kV {
-    my $provider = shift;
+    my $self = shift;
 
     # XXX table name conventions and table management?
 
-    my $dbh = $provider->dbh;
-    $dbh->do("create table dbit_dbm_test1 (k text, V integer)");
-    $dbh->do("insert into dbit_dbm_test1 values ('foo', 42)");
+    my $table_name = $self->get_dbit_temp_name("select_1r2c_si");
+
+    my $dbh = $self->dbh;
+    $dbh->do("create table $table_name (k text, V integer)");
+    $dbh->do("insert into $table_name values ('foo', 42)");
 
     return Fixture->new(
-        statement => "select k, V from dbit_dbm_test1",
+        statement => "select k, V from $table_name",
         demolish => sub {
-            $provider->dbh->do("drop table dbit_dbm_test1");
+            $self->dbh->do("drop table $table_name");
         },
     );
 }
--- sandbox/tim/lib/FixtureProvider/GenericBase.pm
+++ sandbox/tim/lib/FixtureProvider/GenericBase.pm
@@ -3,8 +3,30 @@ package FixtureProvider::GenericBase;
 use strict;
 use warnings;
 
+use Carp qw(croak);
+
+
 use Class::Tiny {
     dbh => undef,
 };
+
+
+sub get_dbit_temp_name {
+    my ($self, $name) = @_;
+    croak "invalid name '$name'"
+        unless $name =~ /^\w+$/;
+
+    my $pid = $$;
+    my (undef,undef,$hour,$mday,$mon,$year) = localtime(time);
+    my $yymmddhh = sprintf "%02d%02d%02d%02d", $year % 100, $mon+1, $mday, $hour;
+
+    my $temp_name = sprintf "dbit1_%s_%d__%s", $yymmddhh, $pid, $name;
+
+    return $temp_name;
+}
+# could also provide methods to:
+# - supply a regex that matches the get_dbit_temp_name name
+# - parse a get_dbit_temp_name to extract especially the yymmddhh
+# - drop all get_dbit_temp_name tables older than N hours (0=all)
 
 1;
